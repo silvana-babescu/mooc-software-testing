6efdc1c5712b643770ed667d2400be496b2926a3
silvana-babescu <52282971+silvana-babescu@users.noreply.github.com>
2019-12-05 17:44:44 +0200

    Added test methods for Discount class

--- src/main/java/tudelft/discount/DiscountApplier.java
+++ src/main/java/tudelft/discount/DiscountApplier.java
@@ -1,5 +1,7 @@
 package tudelft.discount;
 
+import java.util.ArrayList;
+import java.util.List;
 public class DiscountApplier {
 
     private ProductDao dao;
@@ -8,16 +10,17 @@ public class DiscountApplier {
         this.dao = dao;
     }
 
-    public void setNewPrices() {
-
-        for(Product product : dao.all()) {
+    public List<Product> setNewPrices() {
+        List<Product>  allProducts = dao.all();
+        for(Product product : allProducts) {
             if(product.getCategory().equals("BUSINESS")) {
-                product.setPrice(product.getPrice() * 0.9);
-            }
-            if(product.getCategory().equals("HOME")) {
                 product.setPrice(product.getPrice() * 1.1);
             }
+            if(product.getCategory().equals("HOME")) {
+                product.setPrice(product.getPrice() * 0.9);
+            }
         }
+        return allProducts;
 
     }
 }
--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,40 @@
 package tudelft.discount;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+
 public class DiscountApplierTest {
+
+    @Test
+    void checkDiscountPrices() {
+
+        Product Table = new Product("Table", 20.0,"HOME");
+        Product Laptop = new Product("Laptop", 300.0,"BUSINESS");
+        Double businessPrice1=0.0, businessPrice2 = 0.0;
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+
+        List<Product> results = Arrays.asList(Table, Laptop);
+        Mockito.when(dao.all()).thenReturn(results);
+
+        DiscountApplier discApply = new DiscountApplier(dao);
+        List<Product> result = discApply.setNewPrices();
+        for (Product p : result)
+        {
+            if(p.getCategory().equals("BUSINESS")) businessPrice1 = p.getPrice();
+            if(p.getCategory().equals("HOME")) businessPrice2 = p.getPrice();
+
+        }
+
+        Assertions.assertEquals(java.util.Optional.of(330.0),java.util.Optional.of(businessPrice1));
+        Assertions.assertEquals(java.util.Optional.of(18.0),java.util.Optional.of(businessPrice2));
+
+    }
 
 }
